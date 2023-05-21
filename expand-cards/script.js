b9ed14c6f239f816d588f9239887d616916d2d14
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-05-21 13:47:14 +0530

    Expand Cards

---
+++ expand-cards/script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll(".panelarvrfv"); // Keep correct class name
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
